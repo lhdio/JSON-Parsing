00d4d0257eadc66d6401d1a35e8cea9f9683a2d8
Harish-Uz-Jaman Mridha Raju <mridha@bs-23.net>
2018-06-22 17:38:19 +0600

    Implement image cache for image extension

--- JSON Parsing/Utils/ImageExtension.swift
+++ JSON Parsing/Utils/ImageExtension.swift
@@ -9,12 +9,19 @@
 import Foundation
 import UIKit
 
+fileprivate let imageCache = NSCache<NSString, UIImage>()
+
 extension UIImageView {
     func loadImage(urlString: String) {
-        if let url = URL(string: urlString) {
+        if let imageFromCache = imageCache.object(forKey: urlString as NSString) {
+            self.image = imageFromCache
+            return
+        }
+        if let url = URL(string: urlString as String) {
             DispatchQueue.global().async { [weak self] in
                 if let data = try? Data(contentsOf: url) {
                     if let image = UIImage(data: data) {
+                        imageCache.setObject(image, forKey: urlString as NSString)
                         DispatchQueue.main.async {
                             self?.image = image
                         }
