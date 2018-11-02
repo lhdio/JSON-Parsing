459012fddb6c6dc7274051e6b3ec8f6c7bfa9ac6
Harish-Uz-Jaman Mridha Raju <mridha@bs-23.net>
2018-11-02 16:50:25 +0600

    Introduce Protocol and Generics for table view to generalize, simple and safe

---
+++ JSON Parsing/Protocols/TableViewProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewProtocol.swift
+//  JSON Parsing
+//
+//  Created by Raju on 11/2/18.
+//  Copyright © 2018 rajubd49. All rights reserved.
+//
+
+import Foundation
---
+++ JSON Parsing/View/MovieCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.swift
+//  JSON Parsing
+//
+//  Created by Raju on 11/2/18.
+//  Copyright © 2018 rajubd49. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionCell: UICollectionViewCell {
+    
+}
